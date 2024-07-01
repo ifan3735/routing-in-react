efa1e000ff9e1592da87941c01a75e9292ea4c75
ifan3735 <ifanmaina@gmail.com>
2024-07-01 17:06:30 +0300

    second commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Home from './pages/Home.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Error from './pages/Error.tsx'
+import Register from './pages/Register.tsx'
+import Contact from './pages/Contact.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+import Users from './components/darshboard/Users.tsx'
+import UserProfiles from './components/darshboard/UserProfiles.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+    errorElement:<Error/>,
+  },
+  {
+    path: 'register',
+    element: <Register />,
+    errorElement:<Error/>,
+  },
+  {
+    path: 'contact',
+    element: <Contact />,
+    errorElement:<Error/>,
+  },
+  {
+    path: 'dashboard',
+    element: <Dashboard />,
+    errorElement:<Error/>,
+    children: [
+      {
+        path: "users",
+        element: <Users />,
+      },
+      {
+        path: "user-profiles",
+        element: <UserProfiles />,
+      }
+    ]
+  },
+
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
